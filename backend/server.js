b5c2c16019e5c02cfc6508f715713e8977fadc0c
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2019-02-07 20:34:20 -0200

    Completado server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 // server.js
 const app = require('./app');
 const port = process.env.PORT || 3000;
+// port is 3000 unless defined by environment variable
+const host = process.env.HOST || '0.0.0.0';
+
+var server = app.listen(port, host, () => {
+    console.log("Express is listening on " + host + ":" + port);
+});
